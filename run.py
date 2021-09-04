8fdab52c5b7254144a21ffcfcc73aaf885f5b42b
claudiahW <murungiclaud@gmail.com>
2021-09-04 23:14:56 +0300

    create save user function

--- run.py
+++ run.py
@@ -3,7 +3,13 @@ from user import user
 
 def create_user(username,password):
     '''
-    Function to create a new contact
+    Function to create a new user
     '''
     new_user = user(username,password)
     return new_user
+
+def save_user(user):
+    '''
+    Function to save user
+    '''
+    user.save_user()    
