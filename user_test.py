b3fda346f2f4896466f88c73932360fa171b2188
claudiahW <murungiclaud@gmail.com>
2021-09-04 18:35:40 +0300

    create a test method

--- user_test.py
+++ user_test.py
@@ -13,7 +13,20 @@ def setUp(self):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_user = User("Claudia","Wanjiru","0712345678","claudia@ms.com") # create contact object
+        self.new_user = User("Claudia","Wanjiru","1234") # create contact object
 
 
+def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"Claudia")
+        self.assertEqual(self.new_user.last_name,"Wanjiru")
+        self.assertEqual(self.new_user.password,"1234")
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
     
