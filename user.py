b93eacbb09bf3ef7e6c446f3e32aabdc4284253c
claudiahW <murungiclaud@gmail.com>
2021-09-04 18:11:10 +0300

    add function to allow save user information

--- user.py
+++ user.py
@@ -4,3 +4,14 @@ class that generates a new user instance
     '''
 user_list =[]
 
+def __init__(self,user_name,password):
+    self.user_name = user_name
+    self.password = password
+
+def save_user(self):
+    '''
+    saves users information
+
+    '''  
+     
+
