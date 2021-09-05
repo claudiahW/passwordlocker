d745c8da12b81fb5d51fb667c559751a2162cc4a
claudiahW <murungiclaud@gmail.com>
2021-09-05 06:47:41 +0300

    add username and password credentials

--- run.py
+++ run.py
@@ -19,6 +19,18 @@ def main():
         print("Welcome to password locker!")
         print('\n')
         print("select a short code to use for navigation.to create a new user use 'nu':to login use lg:to exit  use ex:")
+        short_code = input().lower()
+        print('\n')
+
+    if short_code =="nu":
+        print('create username')
+        created_user_name = input()
+
+        print('create password')
+        created_user_password =input()
         
+            
+
+
 
         
